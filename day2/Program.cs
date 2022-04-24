4b3828af0dca4b2983b229d0804e3a006f1bf809
Dean Esterhuizen <dean.esterhuizen@openvantage.co.za>
2022-04-24 12:29:43 +0200

    Added an Interface for reuse

--- day2/Program.cs
+++ day2/Program.cs
@@ -1,6 +1,14 @@
 ﻿using day2;
 
 var passwords = File.ReadAllLines("input.txt").ToList();
+
+
+var policy1Validator = new Policy1Validator();
+var policy2Validator = new Policy2Validator();
+File.ReadAllLines("input.txt").Select(x => policy1Validator.Validate(x)).Count(); 
+
+Console.WriteLine($"Valid Passwords: {File.ReadAllLines("input.txt").Where(x => policy1Validator.Validate(x)).Count()}");
+Console.WriteLine($"Valid Passwords: {File.ReadAllLines("input.txt").Where(x => policy2Validator.Validate(x)).Count()}");
 Console.WriteLine($"Valid Passwords: {Solutions.Solve1(passwords)}");
 Console.WriteLine($"Valid Passwords: {Solutions.Solve2(passwords)}");
 
--- day2/Solutions.cs
+++ day2/Solutions.cs
@@ -1,12 +1,59 @@
 namespace day2
 {
+
+    public class PasswordData
+    {
+        public PasswordData(string input)
+        {
+            Policy = input.Split(":").First();
+            Password = input.Split(":").Last().Trim();
+            Digit1 = int.Parse(Policy.Split("-").First());
+            Digit2 = int.Parse(Policy.Split("-").Last().Split(" ").First());
+            RequiredChar = Policy.Split("-").Last().Split(" ").Last();
+        }
+
+        public string? Policy { get; set; }
+        public string? Password { get; set; }
+        public int Digit1 { get; set; }
+        public int Digit2 { get; set; }
+        public string? RequiredChar { get; set; }
+    }
+
+    public interface IValidator
+    {
+        bool Validate(string input);
+    }
+
+    public class Policy1Validator : IValidator
+    {
+        public bool Validate(string input)
+        {
+            var passwordData = new PasswordData(input);
+
+            return passwordData.Password?.Count(x => x.ToString() == passwordData.RequiredChar) >= passwordData.Digit1
+                && passwordData.Password?.Count(x => x.ToString() == passwordData.RequiredChar) <= passwordData.Digit2;
+        }
+    }
+
+    public class Policy2Validator : IValidator
+    {
+        public bool Validate(string input)
+        {
+            var passwordData = new PasswordData(input);
+
+            return passwordData.Password?[passwordData.Digit1 - 1].ToString() == passwordData.RequiredChar
+                ^ passwordData.Password?[passwordData.Digit2 - 1].ToString() == passwordData.RequiredChar;
+        }
+    }
+
     public class Solutions
     {
         public static int Solve1(List<string> passwords)
         {
             //Valid Example 1-3 a: abcde
             var lsPasswordChecks = passwords
-                .Select(p => {
+                .Select(p =>
+                {
 
                     var policy = p.Split(":").First();
                     var password = p.Split(":").Last().Trim();
@@ -14,11 +61,13 @@ namespace day2
                     var maxNumber = int.Parse(policy.Split("-").Last().Split(" ").First());
                     var requiredChar = policy.Split("-").Last().Split(" ").Last();
 
-                    return new { 
-                        Policy = policy, 
-                        Password = password, 
-                        Valid = password.Count(x => x.ToString() == requiredChar) >= minNuber && password.Count(x => x.ToString() == requiredChar) <=  maxNumber };
-                }); 
+                    return new
+                    {
+                        Policy = policy,
+                        Password = password,
+                        Valid = password.Count(x => x.ToString() == requiredChar) >= minNuber && password.Count(x => x.ToString() == requiredChar) <= maxNumber
+                    };
+                });
 
             return lsPasswordChecks.Where(x => x.Valid).Count();
         }
@@ -28,7 +77,8 @@ namespace day2
         {
             //Valid Example 1-3 a: abcde
             var lsPasswordChecks = passwords
-                .Select(p => {
+                .Select(p =>
+                {
 
                     var policy = p.Split(":").First();
                     var password = p.Split(":").Last().Trim();
@@ -36,12 +86,14 @@ namespace day2
                     var unMatchedLocation = int.Parse(policy.Split("-").Last().Split(" ").First());
                     var requiredChar = policy.Split("-").Last().Split(" ").Last();
 
-                    return new { 
-                        Policy = policy, 
-                        Password = password, 
-                        Valid = (password[matchedLocation - 1].ToString() == requiredChar  || password[unMatchedLocation - 1].ToString() == requiredChar) && 
-                        !(password[matchedLocation - 1].ToString() == requiredChar  && password[unMatchedLocation - 1].ToString() == requiredChar) };
-                }); 
+                    return new
+                    {
+                        Policy = policy,
+                        Password = password,
+                        Valid = (password[matchedLocation - 1].ToString() == requiredChar || password[unMatchedLocation - 1].ToString() == requiredChar) &&
+                        !(password[matchedLocation - 1].ToString() == requiredChar && password[unMatchedLocation - 1].ToString() == requiredChar)
+                    };
+                });
 
             return lsPasswordChecks.Where(x => x.Valid).Count();
         }

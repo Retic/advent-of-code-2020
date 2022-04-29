20a652a1bf0ac2df4c08010a812102bc684583a2
Dean Esterhuizen <dean.esterhuizen@openvantage.co.za>
2022-04-29 12:28:45 +0200

    Solved day5 part 2

--- day5/Program.cs
+++ day5/Program.cs
@@ -3,6 +3,21 @@
 var boardingService = new BoardingPassService();
 var data = File.ReadAllLines("input.txt");
 
-var boardingPasses = data.Select(l => boardingService.ParseBoardingPass(l)).ToList();
+var boardingPasses = data.Select(l => boardingService.ParseBoardingPass(l)).ToList().OrderBy(s => s.SeatId);
+var seatIds = boardingPasses.Select(bp => bp.SeatId).ToList();
+
+var previousSeatId = 0;
+
+foreach (var seatId in seatIds)
+{
+  
+    if (previousSeatId != 0 && seatId - previousSeatId > 1)
+    {
+        Console.WriteLine($"Found your Seat: {seatId - 1}");
+    }
+
+    previousSeatId = seatId;
+
+}
 
 Console.WriteLine($"Highest Seat Number: {boardingPasses.Max(x => x.SeatId)}");
